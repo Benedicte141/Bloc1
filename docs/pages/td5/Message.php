5d658c1e2015b1f462619861b6ec2ea38519cecb
Benedicte141 <benedicte.gady@sts-sio-caen.info>
2024-02-05 19:58:49 +0100

    TD5

---
+++ docs/pages/td5/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+  
+<form method="GET" action="resultats.php">
+    <label for="search">Rechercher sur mon site : </label><input type="text" name="search" id="search">
+    <input type="submit" value="Rechercher">
+</form>
+
+
+</body>
+</html>
